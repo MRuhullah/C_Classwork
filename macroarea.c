2655213f65862b32bd20c4fa270fdfe1f5341246
MRuhullah <ateebqazim11@gmail.com>
2025-10-04 16:28:51 +0530

    Update

--- macroarea.c
+++ macroarea.c
@@ -4,23 +4,21 @@
 #define ar_cir(r) (PI * (r) * (r))
 #define ar_tri(b, h) (0.5 * (b) * (h))
 
-int main() {
+int main()
+{
     float side, radius, base, height;
-     
+
     printf("Enter side of square: ");
     scanf("%f", &side);
-    printf("Area of square is = %.2f\n",  ar_sq(side));
+    printf("Area of square is = %.2f\n", ar_sq(side));
 
-   
     printf("Enter radius of circle: ");
     scanf("%f", &radius);
-    printf("Area of circle is = %.2f\n", ar_cir (radius));
-    
-    
+    printf("Area of circle is = %.2f\n", ar_cir(radius));
+
     printf("Enter base and height of triangle: ");
     scanf("%f%f", &base, &height);
     printf("Area of triangle is = %.2f\n", ar_tri(base, height));
-
 
     return 0;
 }
